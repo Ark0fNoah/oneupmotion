328d8bae381e787db16e0c3ac7e1e37f3632603c
ArkOfNoah <designio010@gmail.com>
2026-05-30 00:39:08 +0200

    Add CTA section

---
+++ wp-content/themes/oneup-motion/template-parts/section-cta.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * CTA section.
+ *
+ * @package OneUpMotion
+ */
+?>
+<section class="cta section" aria-labelledby="cta-title">
+	<div class="cta__inner section__inner">
+		<h2 id="cta-title"><?php echo esc_html__( 'Ready to level up your next idea?', 'oneup-motion' ); ?></h2>
+		<a class="button button--dark" href="mailto:hello@oneupmotion.com"><?php echo esc_html__( 'Get in Touch', 'oneup-motion' ); ?></a>
+	</div>
+</section>
