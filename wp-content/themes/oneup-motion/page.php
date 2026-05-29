5ef3976a38d683dc0999fcbb67daff16648cee99
ArkOfNoah <designio010@gmail.com>
2026-05-30 01:36:28 +0200

    Render OneUp Sections on pages

--- wp-content/themes/oneup-motion/page.php
+++ wp-content/themes/oneup-motion/page.php
@@ -7,13 +7,26 @@
 
 get_header();
 ?>
-<main id="main" class="site-main content-wrap">
-	<?php while ( have_posts() ) : the_post(); ?>
+
+<?php
+while ( have_posts() ) :
+	the_post();
+	if ( function_exists( 'oum_has_sections' ) && oum_has_sections( get_the_ID() ) ) :
+		?>
+		<main id="main" class="site-main">
+			<?php oum_render_sections( get_the_ID() ); ?>
+		</main>
+	<?php else : ?>
+		<main id="main" class="site-main content-wrap">
 		<article <?php post_class( 'entry-content' ); ?>>
-			<header class="entry-header"><h1><?php the_title(); ?></h1></header>
+			<header class="entry-header">
+				<h1><?php the_title(); ?></h1>
+			</header>
 			<?php the_content(); ?>
 		</article>
-	<?php endwhile; ?>
-</main>
+		</main>
+	<?php endif; ?>
+<?php endwhile; ?>
+
 <?php
 get_footer();
