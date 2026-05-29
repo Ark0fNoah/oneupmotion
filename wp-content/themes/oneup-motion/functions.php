900564a97ae4a97c3cd139d6059278baa97b1c24
ArkOfNoah <designio010@gmail.com>
2026-05-30 00:35:46 +0200

    Add theme bootstrap

---
+++ wp-content/themes/oneup-motion/functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * OneUp Motion theme bootstrap.
+ *
+ * @package OneUpMotion
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$oum_includes = array(
+	'inc/helpers.php',
+	'inc/setup.php',
+	'inc/menus.php',
+	'inc/assets.php',
+	'inc/customizer.php',
+);
+
+foreach ( $oum_includes as $oum_file ) {
+	$oum_path = get_template_directory() . '/' . $oum_file;
+
+	if ( file_exists( $oum_path ) ) {
+		require_once $oum_path;
+	}
+}
